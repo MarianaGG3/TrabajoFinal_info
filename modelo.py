bc7d4664221b5ac2a29e53b948bf53882323fcc3
MarianaGG3 <mariana.giraldog@udea.edu.co>
2024-05-23 00:22:16 -0500

    clase imagen

---
+++ modelo.py
@@ -0,0 +1,14 @@
+class Imagen():
+    def __init__(self):
+        self.__numero_imagen=int
+        self.__usuario=""
+
+
+    def asignarNumeroImagen(self):
+        pass
+    def asignarUsuario(self):
+        pass
+    def verNumeroImagen(self):
+        pass
+    def verUsuario(self):
+        pass
