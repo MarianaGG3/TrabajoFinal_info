0f745e74594ed74c1dff52db31f5c6b93e59b540
sarapalaciod <sara.palaciod@udea.edu.com>
2024-06-03 16:54:40 -0500

    modificacion conteo de imagen

--- modelo.py
+++ modelo.py
@@ -36,7 +36,7 @@ class Imagen:
         Umb,img=cv2.threshold(imgB ,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         R=cv2.medianBlur(img, 5)
 
-        kernel= np.ones((15,15),np.uint8)
+        kernel= np.ones((1,1),np.uint8)
         imaOp=cv2.morphologyEx(R, cv2.MORPH_CLOSE, kernel, iterations = 1)
 
         elem,mask=cv2.connectedComponents(imaOp)
